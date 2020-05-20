b4ca9be74a42ed70f24880d5e5e8d2600932bbbc
deroffal <32491733+deroffal@users.noreply.github.com>
2020-05-20 19:15:25 +0200

    Export csv local

--- src/main/kotlin/fr/deroffal/export/Installations.kt
+++ src/main/kotlin/fr/deroffal/export/Installations.kt
@@ -2,8 +2,11 @@ package fr.deroffal.export
 
 import java.time.LocalDate
 
-data class Texte(val dateDoc: LocalDate?, val typeDoc: String?, val descriptionDoc: String?, val urlDoc: String?)
-data class Site(
+data class Texte(val dateDoc: LocalDate?, val typeDoc: String?, val descriptionDoc: String?, val urlDoc: String?) {
+    fun isNotEmpty() = listOfNotNull(dateDoc, typeDoc, descriptionDoc, urlDoc).isNotEmpty()
+}
+
+data class EtablissementCsv(
     val numeroInspection: String,
     val nom: String,
     val codePostal: String,
@@ -13,6 +16,33 @@ data class Site(
     val statutSeveso: String,
     val etatActivite: String,
     val prioriteNationale: String,
-    val iedMtd: String,
-    val textes: MutableList<Texte> = mutableListOf()
+    val iedMtd: String
+) {
+    fun getNumeroEtablissement() = numeroInspection.replace('.', '-')
+}
+
+data class Etablissement(
+    val idInst: String,
+    val nomInst: String,
+    val codeSiret: String?,
+    val regionInst: String,
+    val departementInst: String,
+    val communeInst: String,
+    val adresse1: String?,
+    val adresse2: String?,
+    val codePostal: String,
+    val codeInsee: String,
+    val x: String,
+    val y: String,
+    val systProj: String,
+    val codeActiviteInst: String?,
+    val activiteInst: String?,
+    val etatActiviteInst: String?,
+    val nomServiceInspection: String,
+    val numInspection: String,
+    val derInspection: String?,
+    val regimeInst: String,
+    val prioNational: String,
+    val pied: String,
+    val statutInst: String
 )
--- src/main/kotlin/fr/deroffal/export/Main.kt
+++ src/main/kotlin/fr/deroffal/export/Main.kt
@@ -3,61 +3,92 @@ package fr.deroffal.export
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import java.io.FileWriter
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
+
+const val paramPaysDeLaLoire = "region=52"
+const val paramLoireAtlantique = "departement=44"
+const val paramNantes = "commune=44109"
+const val baseUrl = "http://www.georisques.gouv.fr/webappReport/ws/installations"
+
+val httpBuilder = HttpBuilder()
+val mapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
 fun main(args: Array<String>) {
 
-    val httpClient = HttpClient.newBuilder().build()
-
     val now = LocalDate.now().format(DateTimeFormatter.ISO_DATE)
-//    val loireAtlantique = 52
-    val nantes = 44109
 
-    val requestBuilder =
-        HttpRequest.newBuilder()
-//        HttpRequest.newBuilder(URI.create("http://www.georisques.gouv.fr/webappReport/ws/installations/sitesdetails/detailsites_${now}.csv?etablissement=&region=${loireAtlantique}&isExport=true"))
-            .GET()
-            .setHeader("User-Agent", "export_georisques_donnees_icpe")
+    val etablissements =
+        httpBuilder.getAsString("$baseUrl/sitesdetails/detailsites_$now.csv?etablissement=&$paramPaysDeLaLoire&isExport=true")
+            .split("\r\n").filterNot { it.isEmpty() }.drop(1)
+            .map { it.split(";") }
+            .map {
+                EtablissementCsv(
+                    numeroInspection = it[0],
+                    nom = it[1],
+                    codePostal = it[2],
+                    commune = it[3],
+                    departement = it[4],
+                    regimeEnVigueur = it[5],
+                    statutSeveso = it[6],
+                    etatActivite = it[7],
+                    prioriteNationale = it[8],
+                    iedMtd = it[9]
+                )
+            }
 
-    val httpResponse = httpClient.send(requestBuilder.uri(URI.create("http://www.georisques.gouv.fr/webappReport/ws/installations/sitesdetails/detailsites_${now}.csv?etablissement=&commune=${nantes}&isExport=true")).build(), HttpResponse.BodyHandlers.ofString())
+    println("${etablissements.size} établissements trouvés...")
 
-    val body = httpResponse.body().split("\r\n").filterNot { it.isEmpty() }.drop(1)
-        .map { it.split(";") }
-        .map {
-            Site(
-                numeroInspection = it[0],
-                nom = it[1],
-                codePostal = it[2],
-                commune = it[3],
-                departement = it[4],
-                regimeEnVigueur = it[5],
-                statutSeveso = it[6],
-                etatActivite = it[7],
-                prioriteNationale = it[8],
-                iedMtd = it[9]
+    val lignesCSV = etablissements.map {
+        val textes = recupererTextes(it)
+        val etablissement = recupererEtablissement(it)
+
+        listOf(
+            it.numeroInspection,
+            it.nom,
+            it.codePostal,
+            it.commune,
+            it.departement,
+            it.regimeEnVigueur,
+            it.statutSeveso,
+            it.etatActivite,
+            it.prioriteNationale,
+            it.iedMtd,
+            etablissement.activiteInst
+        ) + textes.map {
+            listOf(
+                it.dateDoc?.format(DateTimeFormatter.ISO_DATE),
+                it.typeDoc,
+                it.descriptionDoc,
+                it.urlDoc
             )
-        }
+        }.flatten()
+    }
+    val fileWriter = FileWriter("export_${Date().time}.csv")
 
 
-    val mapper = jacksonObjectMapper().registerModule(JavaTimeModule())
-
-    body.onEach {
-        val numeroInspection = it.numeroInspection.replace('.', '-')
-        val textesRequest =
-            requestBuilder.uri(URI("http://www.georisques.gouv.fr/webappReport/ws/installations/etablissement/$numeroInspection/texte"))
-                .build()
-        val textesResponse = httpClient.send(textesRequest, HttpResponse.BodyHandlers.ofString())
-        val content = textesResponse.body()
-        val textes = mapper.readValue<Collection<Texte>>(content).sortedBy { it.dateDoc }
-        it.textes.addAll(textes)
-    }.filter { it.textes.isNotEmpty() }
+    val entete =
+        "Numéro d'inspection;Nom établissement;Code postal;Commune;Département;Régime en vigueur;Statut SEVESO;Etat d’activité;Priorité nationale;IED-MTD;Activité" + (1..24).map { ";Date document;Type document;Description document;URL document" }
+            .joinToString(postfix = "\n")
+    fileWriter.write(entete)
+    lignesCSV
+        .map { it.joinToString(separator = ";", postfix = "\n") { it?.replace(';', ',') ?: "" } }
         .forEach {
-            println(it.nom)
+            fileWriter.write(it)
         }
-    println(body)
+}
+
+private fun recupererTextes(etablissementCsv: EtablissementCsv): List<Texte> {
+    val texteStr = httpBuilder.getAsString("$baseUrl/etablissement/${etablissementCsv.getNumeroEtablissement()}/texte")
+    return mapper
+        .readValue<Collection<Texte>>(texteStr)
+        .filter { it.isNotEmpty() }
+        .sortedBy { it.dateDoc }.reversed()
+}
+
+private fun recupererEtablissement(etablissementCsv: EtablissementCsv): Etablissement {
+    val etablissementStr = httpBuilder.getAsString("$baseUrl/etablissement/${etablissementCsv.getNumeroEtablissement()}")
+    return mapper.readValue(etablissementStr)
 }
